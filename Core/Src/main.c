36d0310f5ab9ac05a602ce5e46bebc68e2ac84ff
ASattar04 <sattarahmed204@gmail.com>
2023-11-13 19:43:20 -0500

    Worked on code for temperature sensor reading

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -128,6 +128,55 @@ int16_t GYRO_READ_DIR(char dir){
 
 //}
 
+/* TEMPERATURE SENSOR CODE */
+
+static const uint8_t TMP102_ADDR = 0x48 << 1; // 8 bit Address
+static const uint8_t REG_TEMP = 0x00;
+
+float Read_Temperature()
+{
+
+	HAL_StatusTypeDef temp_status;
+	uint8_t buf[12];
+	uint16_t rawTemp;
+	float tempFloat;
+
+	// Tell TMP102 that we want to read from the temperature register
+	buf[0] = REG_TEMP;
+	temp_status = HAL_I2C_Master_Transmit(&hi2c1, TMP102_ADDR, buf, 1, HAL_MAX_DELAY);
+	if (temp_status != HAL_OK)
+	{
+		strcpy((char*))buf, "ERROROROR transmit");
+	}else{
+
+		// Read 2 bytes from the temperature register
+		temp_status = HAL_I2C_Master_Receive(&hi2c1, TMP102_ADDR, buf, 2, HAL_MAX_DELAY);
+		if (temp_status != HAL_OK){
+			strcpy((char*)buf, "EOROREOROROR receive");
+		}else{
+
+			// Combine the bytes
+			rawTemp = ((int16_t)buf[0] << 4 | buf[1] >> 4);
+
+			// Convert to 2's complement, since temperature can be negative
+			if (val > 0x7FF){
+
+				val |= 0xF000;
+
+			}
+
+			// Convert to float temperature value (Celsius)
+			tempFloat = val * 0.0625;
+			return tempFloat;
+
+		}
+
+	}
+	return 129.0;
+
+}
+/* TEMPERATURE SENSOR CODE END :) */
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
